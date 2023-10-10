f793969802517b46bb43cefcc699b7a1619c1368
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-10-10 15:26:14 +0100

    create routes for the application

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -3,6 +3,36 @@ const router = express.Router();
 const data = {}
 
 
+data.employees = require('../../data/employees.json')
+    router.route('/')
+        .get((req, res) => {
+            res.json(data.employees)
+        })
 
+        .post((req, res) => {
+            res.json({
+                "firstname": req.body.firstname, 
+                "lastname": req.body.lastname
+            })
+        })
+
+        .put((req, res) => {
+            res.json({
+                "firstname": req.body.firstname, 
+                "lastname": req.body.lastname
+            })
+        })
+
+        delete((req, res) => {
+            res.json({"id" : req.body.id})
+        })
+
+        router.route('/ id')
+        .get((req, res) => {
+            res.json({"id" : req.params.id})
+        })
+
+
+module.exports = router;
 
 
